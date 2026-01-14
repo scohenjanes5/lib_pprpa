38f7680bb3781c085e6f67ce8f2d21b497c1c54a
scohenjanes5 <101665992+scohenjanes5@users.noreply.github.com>
2026-01-14 12:04:16 -0500

    Lock energy units to PySCF data

--- lib_pprpa/pprpa_davidson.py
+++ lib_pprpa/pprpa_davidson.py
@@ -747,7 +747,7 @@ def _pprpa_print_eigenvector(
     tri_row_v, tri_col_v = np.tril_indices(nvir, is_singlet-1)
 
     nroot = len(exci)
-    au2ev = 27.211386
+    from pyscf.data.nist import HARTREE2EV
     f_vals = []
     tdm_vals = []
     vees = []
@@ -757,7 +757,7 @@ def _pprpa_print_eigenvector(
             vee = exci[iroot] - exci0
             print("#%-d %s excitation:  exci= %-12.6f  eV   2e=  %-12.6f  eV" %
                   (iroot + 1, multi,
-                   vee * au2ev, exci[iroot] * au2ev))
+                   vee * HARTREE2EV, exci[iroot] * HARTREE2EV))
             if mo_dip is not None:
                 f, tdm = get_pprpa_oscillator_strength(
                     nocc=nocc, mo_dip=mo_dip, channel=channel,
@@ -789,7 +789,7 @@ def _pprpa_print_eigenvector(
             vee = exci[iroot] - exci0
             print("#%-d %s de-excitation:  exci= %-12.6f  eV   2e=  %-12.6f  eV" %
                   (iroot + 1, multi,
-                   vee * au2ev, exci[iroot] * au2ev))
+                   vee * HARTREE2EV, exci[iroot] * HARTREE2EV))
             if mo_dip is not None:
                 f, tdm = get_pprpa_oscillator_strength(
                     nocc=nocc, mo_dip=mo_dip, channel=channel,
@@ -818,7 +818,7 @@ def _pprpa_print_eigenvector(
 
     f_vals = np.array(f_vals)
     tdm_vals = np.array(tdm_vals)
-    vees = np.array(vees) * au2ev
+    vees = np.array(vees) * HARTREE2EV
     if len(f_vals) > 0 and np.sum(f_vals) > 0.0:
         return tdm_vals, vees
     else:
--- lib_pprpa/upprpa_direct.py
+++ lib_pprpa/upprpa_direct.py
@@ -201,13 +201,13 @@ def _pprpa_print_eigenvector(subspace, nocc, nvir, nocc_fro, thresh, hh_state,
         tri_row_o, tri_col_o = numpy.tril_indices(nocc, -1)
         tri_row_v, tri_col_v = numpy.tril_indices(nvir, -1)
 
-    au2ev = 27.211386
+    from pyscf.data.nist import HARTREE2EV
 
     # =====================> two-electron removal <======================
     for istate in range(min(hh_state, oo_dim)):
         print("#%-d %s de-excitation:  exci= %-12.6f  eV   2e=  %-12.6f  eV" %
-              (istate + 1, subspace[:2], (exci[oo_dim-istate-1] - exci0) * au2ev,
-               exci[oo_dim-istate-1] * au2ev))
+              (istate + 1, subspace[:2], (exci[oo_dim-istate-1] - exci0) * HARTREE2EV,
+               exci[oo_dim-istate-1] * HARTREE2EV))
         if subspace == 'aaaa' or subspace == 'bbbb':
             full = numpy.zeros(shape=[nocc, nocc], dtype=numpy.double)
             full[tri_row_o, tri_col_o] = xy[oo_dim-istate-1][:oo_dim]
@@ -244,8 +244,8 @@ def _pprpa_print_eigenvector(subspace, nocc, nvir, nocc_fro, thresh, hh_state,
     # =====================> two-electron addition <=====================
     for istate in range(min(pp_state, vv_dim)):
         print("#%-d %s excitation:  exci= %-12.6f  eV   2e=  %-12.6f  eV" %
-              (istate + 1, subspace[:2], (exci[oo_dim+istate] - exci0) * au2ev,
-               exci[oo_dim+istate] * au2ev))
+              (istate + 1, subspace[:2], (exci[oo_dim+istate] - exci0) * HARTREE2EV,
+               exci[oo_dim+istate] * HARTREE2EV))
         if subspace == 'aaaa' or subspace == 'bbbb':
             full = numpy.zeros(shape=[nocc, nocc], dtype=numpy.double)
             full[tri_row_o, tri_col_o] = xy[oo_dim+istate][:oo_dim]
