d364e43bce9b79adf4e192a0d59b4132e7c74948
scohenjanes5 <101665992+scohenjanes5@users.noreply.github.com>
2026-01-26 15:35:05 -0500

    update example to use updated helper functions

--- examples/in_pyscf/gradients/pprpa_grad_gamma.py
+++ examples/in_pyscf/gradients/pprpa_grad_gamma.py
@@ -3,13 +3,8 @@ r"""
 """
 import pyscf
 import numpy as np
-from pyscf.pbc import gto, scf, dft
-from lib_pprpa.pprpa_davidson import ppRPA_Davidson
-
-mult = "t"
-nfrozen_occ = 0
-vir_cut = 1e5
-istate = 0
+from pyscf.pbc import dft
+from lib_pprpa.grad.ase_utils import pprpaobj
 
 def mfobj(dx):
     cell = pyscf.M(
@@ -25,44 +20,20 @@ def mfobj(dx):
     precision = 1e-12,
     verbose = 5
 )
-    
+
     # For Gamma-point gradient calculations, one must use FFTDF with pseudopotential.
     # mf = scf.RHF(cell)
     mf = dft.RKS(cell,xc="pbe")
     mf.exxdiv = None
     mf.conv_tol = 1e-12
     return mf
-def pprpaobj(mf, nfrozen_occ, vir_cut, mo_eri=True):
-    mo_ene = mf.mo_energy
-    mol = mf.mol
-    import numpy
-    nvircut = numpy.sum(mo_ene > vir_cut)
-    nvir_act = mo_ene.shape[0] - nvircut - mol.nelectron//2
-    nocc = mol.nelectron//2 - nfrozen_occ
-    nmo = nocc + nvir_act
-    mo_energy = mo_ene[nfrozen_occ:mol.nelectron//2+nvir_act]
-    mo_coeff = mf.mo_coeff[:,nfrozen_occ:mol.nelectron//2+nvir_act]
-    
-    pprpa = ppRPA_Davidson(nocc, mo_energy, Lpq=None, channel="pp", nroot=3, residue_thresh=1e-12)
 
-    # One can use either the MO eri or the ao direct approach.
-    # For small active spaces, MO eri should be faster.
-    if mo_eri:
-        eri = mf.with_df.get_mo_eri(mo_coeff, compact=False)
-        eri = eri.reshape(nmo, nmo, nmo, nmo).transpose(0, 2, 1, 3)
-        vvvv = np.ascontiguousarray(eri[nocc:, nocc:, nocc:, nocc:])
-        oovv = np.ascontiguousarray(eri[:nocc, :nocc, nocc:, nocc:])
-        oooo = np.ascontiguousarray(eri[:nocc, :nocc, :nocc, :nocc])
-        pprpa.use_eri(vvvv, oovv, oooo)
-    else:
-        pprpa._ao_direct = True
-        pprpa._scf = mf
-    
-    pprpa.mu = 0.0
-    return pprpa
-
-
-
+mult = "t"
+nfrozen_occ = 0
+vir_cut = 1e5
+istate = 0
+channel = "pp"
+params = {"nroot": 3, "residue_thresh": 1e-12, "mo_eri": True, "nfrozen_occ": nfrozen_occ, "vir_cut": vir_cut}
 
 dx = 0.0001
 mf1 = mfobj(dx)
@@ -80,14 +51,14 @@ h2 = mf2.get_hcore()[0]
 dh = (h1 - h2)/(2.0*dx)
 e_hf = (e1 - e2)/dx/2.0
 
-mp1 = pprpaobj(mf1, nfrozen_occ, vir_cut)
+mp1 = pprpaobj(mf1, channel, **params)
 mp1.kernel(mult)
 mp1.analyze()
 if mult == "s":
     e1 = mp1.exci_s[istate]
 else:
     e1 = mp1.exci_t[istate]
-mp2 = pprpaobj(mf2, nfrozen_occ, vir_cut)
+mp2 = pprpaobj(mf2, channel, **params)
 mp2.kernel(mult)
 mp2.analyze()
 if mult == "s":
@@ -109,7 +80,7 @@ if mult == "s":
 else:
     oo_dim = nocc * (nocc - 1) // 2
 
-mp = pprpaobj(mf, nfrozen_occ, vir_cut)
+mp = pprpaobj(mf, channel, **params)
 mp.kernel(mult)
 mp.analyze()
 
